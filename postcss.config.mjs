2d9034927060eedf878e3d1c58ea2681fab335c9
fargila <atilahunesco@gmail.com>
2026-02-07 21:29:53 -0300

    first commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,19 @@
+import { resolve, dirname } from "path"
+import { fileURLToPath } from "url";
+
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
+  resolve: {
+    alias: {
+      '#components': resolve(dirname(fileURLToPath(import.meta.url)), 'app/components'),
+      '#constants': resolve(dirname(fileURLToPath(import.meta.url)), 'app/constants'),
+      '#store': resolve(dirname(fileURLToPath(import.meta.url)), 'app/store'),
+      '#hoc': resolve(dirname(fileURLToPath(import.meta.url)), 'app/hoc'),
+      '#windows': resolve(dirname(fileURLToPath(import.meta.url)), 'app/windows'),
+    }
+  }
 };
 
 export default config;
